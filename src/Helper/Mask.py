f678100698ca0b19fb2b6ea8c96bbaeff6b22dac
Hannah Nelson <nexec64@gmail.com>
2025-06-02 08:55:57 +0000

    continued development of FixedMaskingEncoder, realized that Mask needs to tolerate batches.

--- src/Helper/Mask.py
+++ src/Helper/Mask.py
@@ -1,3 +1,4 @@
+#TODO make this so it works when masking in batches, because it needs to be able to.
 #  import stuff!
 import torch
 # 
@@ -5,47 +6,47 @@ def assert_same_size(a, b, error_msg): #  
     if a.size()!=b.size():
         raise ValueError(error_msg)
 # 
-class mask(): #  
+class Mask(): #  
     def __init__(self, mask_tensor): #  
         #  validate input!
         if not torch.all((mask_tensor==0.0) or (mask_tensor==1.0)):
-            raise ValueError(f'Error in creation of mask, mask_tensor must consist solely of ones and zeroes {mask_tensor=}')
+            raise ValueError(f'Error in creation of Mask, mask_tensor must consist solely of ones and zeroes {mask_tensor=}')
         # 
         #  accept input!
         self.mask_tensor = mask_tensor
         # 
     # 
     def union(self, other_mask): #  
-        """this returns the union the mask with another mask"""
-        assert_same_size(self, other_mask, 'Attempted to union masks of different size')
+        """this returns the union the Mask with another Mask"""
+        assert_same_size(self, other_mask, 'Attempted to union Masks of different size')
         new_mask_tensor = torch.zeros(self.size())
         new_mask_tensor[self.mask_tensor==1.0] = 1.0
         new_mask_tensor[other_mask.mask_tensor==1.0] = 1.0
-        return mask(new_mask_tensor)
+        return Mask(new_mask_tensor)
     # 
     def conjunction(self, other_mask): #  
-        """this returns the conjunction of the mask with another mask"""
-        assert_same_size(self, other_mask, 'Attempted to conjunction masks of different size')
+        """this returns the conjunction of the Mask with another Mask"""
+        assert_same_size(self, other_mask, 'Attempted to conjunction Masks of different size')
         new_mask_tensor = torch.ones(self.size())
         new_mask_tensor[self.mask_tensor==0.0] = 0.0
         new_mask_tensor[other_mask.mask_tensor==0.0] = 0.0
-        return mask(new_mask_tensor)
+        return Mask(new_mask_tensor)
     # 
     def invert(self): #  
-        """returns an inverted copy of the mask"""
+        """returns an inverted copy of the Mask"""
         new_mask_tensor = torch.zeros(self.size())
         new_mask_tensor[self.mask_tensor==0.0] = 1.0
-        return mask(new_mask_tensor)
+        return Mask(new_mask_tensor)
     # 
     def __repr__(self): #  
-        return(f'mask({self.mask_tensor})')
+        return(f'Mask({self.mask_tensor})')
     # 
     def size(self): #  
         return self.mask_tensor.size()
     # 
     def censor(self, tensor): #  
     """
-    Uses the mask to censor a tensor. Takes in a tensor and returns a censored tensor
+    Uses the Mask to censor a tensor. Takes in a tensor and returns a censored tensor
     """
     return_value = tensor.clone()
     return_value[self.mask_tensor==1.0] = 0.0
--- src/Model/FixedMaskingEncoder.py
+++ src/Model/FixedMaskingEncoder.py
@@ -2,18 +2,17 @@
 #TODO make this work by masking off a portion of an image.
 #TODO make this use MaskingSubencoders to do its job
 #TODO make the sampling work with the modified formulation of combined MaskingSubencoders.
-#TODO make a mask inversion function
-#TODO write a masking function
 #TODO write the appropriate guassian combiner function.
 #  setup
 import torch
 from torch import nn
 import logging
+import Mask from Helper.Mask
 logger = logging.getLogger(__name__)
 logger.info('Loading FixedMaskingEncoder')
 # 
 class FixedMaskingEncoder(nn.Module):
-  def __init__(self, *, subencoder, mask=torch.zeros([1, 28, 28])):
+  def __init__(self, *, subencoder, mask=Mask(torch.zeros([1, 28, 28]))):
     super().__init__()
     #  Make sure the subencoder is appropriately sized
     if subencoder.data_shape != mask.shape:
@@ -25,6 +24,8 @@ class FixedMaskingEncoder(nn.Module):
     # 
     self.flatten = nn.Flatten()
   def forward(self, input): #   TODO change this to the appropriate forward function
+    masked_input_1 = mask.censor(input)
+    masked_input_2 = mask.invert().censor(input)
     network_output = self.estack(self.flatten(input))
     mean, network_stdev = torch.split(network_output, self.embedding_dimensionality, dim=1)
     stdev = self.uniform_stdev + torch.abs(network_stdev)
